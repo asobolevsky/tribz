616952525f49a3cd083ab138ddb63dc0110fdc9b
Alexey Sobolevsky <sobolevsky.alexey@gmail.com>
2016-09-05 18:43:12 +0300

    fixes

--- Tribz/ResultPageViewController.swift
+++ Tribz/ResultPageViewController.swift
@@ -13,6 +13,7 @@ class ResultPageViewController: UIViewController {
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var resultTextView: UITextView!
+    @IBOutlet weak var resultBodyCont: UIView!
     @IBOutlet weak var nextStepViewView: UIView!
     
     var userProgress: UserProgress!
@@ -25,11 +26,12 @@ class ResultPageViewController: UIViewController {
         
         let resultText = Result.getResultForPrimaryColor(primaryColor)
         resultTextView.text = resultText
+        resultTextView.textColor = UIColor.whiteColor()
         
         let image = UIImage(named: "screen_8")
         contentView.backgroundColor = UIColor(patternImage: image!)
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(AboutPageViewController.nextStepPressed))
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ResultPageViewController.nextStepPressed))
         nextStepViewView.addGestureRecognizer(tapGesture)
         
         var backgroundColor: UIColor
@@ -44,13 +46,12 @@ class ResultPageViewController: UIViewController {
             backgroundColor = UIColor(red: 0.0549019607843137, green: 0.435294117647059, blue: 0.776470588235294, alpha: 0.95)
         }
         
-        contentView.backgroundColor = backgroundColor
+        resultBodyCont.backgroundColor = backgroundColor
         nextStepViewView.backgroundColor = backgroundColor
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func nextStepPressed() {
+        performSegueWithIdentifier("showSubmitPage", sender: nil)
     }
 }
